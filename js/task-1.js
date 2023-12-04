5ff59565ca4dd1c4fa69bd40d3a0f102ee7188aa
tsouris <nikolastsouris16@gmail.com>
2023-12-04 21:14:23 +0200

    1st task complete

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+// Task 1
+// Напиши скрипт підрахунку вартості гравіювання прикрас. Для цього створи функцію
+// `calculateEngravingPrice(message, pricePerWord)` приймаючу рядок (в рядку будуть
+// тільки слова і прогалини) і ціну гравіювання одного слова, і повертає ціну
+// гравіювання всіх слів в рядку.
+
+const calculateEngravingPrice = function (message, pricePerWord) {
+  return message.split(" ").length * pricePerWord;
+};
+
+console.log(
+  calculateEngravingPrice("Proin sociis natoque et magnis parturient montes mus", 10)
+);
---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+// Task 2
+// Напиши функцію `findLongestWord(string)`, яка приймає параметром довільний рядок
+// (в рядку будуть тільки слова і прогалини) і повертає найдовше слово в цьому
+// рядку.
+
+const findLongestWord = function (string) {
+  let longestWord = "";
+  for (let i = 0; i < string.length; i++) {
+    longestWord += string[i];
+  }
+  return longestWord;
+};
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); 
+
+console.log(findLongestWord("Google do a roll")); 
+
+console.log(findLongestWord("May the force be with you")); 
