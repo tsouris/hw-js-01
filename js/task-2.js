c1d829f31741b97f50c63cb4e90e0bae0468276d
tsouris <nikolastsouris16@gmail.com>
2023-12-05 18:19:11 +0200

    task 2 finished

--- js/task-2.js
+++ js/task-2.js
@@ -4,15 +4,21 @@
 // рядку.
 
 const findLongestWord = function (string) {
+  const words = string.split(" ");
   let longestWord = "";
-//   for (let i = 0; i < string.length; i++) {
-//     longestWord += string[i];
-//   }
-//   return longestWord;
+
+  for (let i = 0; i < words.length; i++) {
+    // Check if the current word is longer than the current longest word
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+
+  return longestWord;
 };
 
-console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); 
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
 
-console.log(findLongestWord("Google do a roll")); 
+console.log(findLongestWord("Google do a roll"));
 
-console.log(findLongestWord("May the force be with you")); 
+console.log(findLongestWord("May the force be with you"));
